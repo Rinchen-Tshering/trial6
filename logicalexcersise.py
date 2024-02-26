f4c0c2f7d4bd49b69429d4d58d51dfc58ebb8606
Rinchen-Tshering <02230104.cst@rub.edu.bt>
2024-02-26 15:23:57 +0600

    ticket discount

---
+++ logicalexcersise.py
@@ -0,0 +1,10 @@
+print("movie ticket discount")
+print("------------------------------------------------------------------------------------------------------")
+
+age = int(input("Enter your age: "))
+student = input("are you a student? (yes/no): ")
+if (age <= 12 or ((age >= 13 or age <= 18) and student == "yes")):
+    print("you are elegible for the movie ticket discount")
+
+else:
+    print("you are not elegible for this offer")
