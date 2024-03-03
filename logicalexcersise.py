e2ec921610d85787fdd3dbb1aa3f45de7cc12b15
Rinchen-Tshering <02230104.cst@rub.edu.bt>
2024-03-03 19:36:59 +0600

    logical exercise

--- logicalexcersise.py
+++ logicalexcersise.py
@@ -3,6 +3,7 @@ print("-------------------------------------------------------------------------
 
 age = int(input("Enter your age: "))
 student = input("are you a student? (yes/no): ")
+
 if (age <= 12 or ((age >= 13 or age <= 18) and student == "yes")):
     print("you are elegible for the movie ticket discount")
 
